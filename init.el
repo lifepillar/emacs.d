9f5642d7a0541dec9f1c6dd618a88fa7f6edd692
Lifepillar <github@lifepillar.com>
2015-12-25 18:35:42 +0200

    Set GUI font.

--- init.el
+++ init.el
@@ -6,3 +6,14 @@
 
 ;; Set up Unicode
 (prefer-coding-system 'utf-8)
+
+;; Set default font (GUI)
+(when (display-graphic-p)
+  (defconst lifepillar-default-font "Menlo")
+
+  (if (find-font (font-spec :name lifepillar-default-font))
+    (set-frame-font (font-spec :family lifepillar-default-font :size 11 :weight 'normal) t t)
+    (message "Cannot find font \"%s\"!" lifepillar-default-font))
+
+  (setq-default line-spacing 2))
+

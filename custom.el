6193a11d7208f87422b579066b4e4187a0eb741c
Lifepillar <github@lifepillar.com>
2015-12-25 18:56:38 +0200

    Enable basic Ido support.

--- custom.el
+++ custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ido-enable-flex-matching t)
+ '(ido-everywhere t)
+ '(ido-mode (quote both) nil (ido))
  '(inhibit-default-init t)
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)

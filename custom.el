0b25a29a7c973ba4f395c6d46c26976cf91f3d15
Lifepillar <github@lifepillar.com>
2015-12-25 19:15:16 +0200

    Do not ask for confirmation when creating a new buffer.

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(confirm-nonexistent-file-or-buffer nil)
+ '(ido-create-new-buffer (quote always))
  '(ido-enable-flex-matching t)
  '(ido-everywhere t)
  '(ido-mode (quote both) nil (ido))

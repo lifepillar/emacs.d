99669d4fa6554594da20dfe86c77ee32271b0c02
Lifepillar <github@lifepillar.com>
2015-12-25 21:48:30 +0200

    Do not create lock files.

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(confirm-nonexistent-file-or-buffer nil)
+ '(create-lockfiles nil)
  '(ido-create-new-buffer (quote always))
  '(ido-enable-flex-matching t)
  '(ido-everywhere t)

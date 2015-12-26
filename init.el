1ed3390e7fce2fa5fc862a06dc9f75b6ac348acd
Lifepillar <github@lifepillar.com>
2015-12-26 18:16:10 +0200

    Add code to measure the time spent for initialization.
    Code stol... borrowed from John Wiegley's Emacs config.

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
 ;; Lifepillar's Emacs Initialization File
+(defconst lifepillar-emacs-start-time (current-time))
+(unless noninteractive
+  (message "Loading %s..." load-file-name))
 
 
 ;; Customization file
@@ -29,4 +32,18 @@
 
 ;; Key bindings
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+
+;;; Post initialization
+(let ((elapsed (float-time (time-subtract (current-time)
+                                          lifepillar-emacs-start-time))))
+  (message "Loading %s...done (%.3fs)" load-file-name elapsed))
+
+(add-hook 'after-init-hook
+          `(lambda ()
+             (let ((elapsed (float-time (time-subtract (current-time)
+                                                       lifepillar-emacs-start-time))))
+               (message "Loading %s...done (%.3fs) [after-init]"
+                        ,load-file-name elapsed)))
+          t)
 

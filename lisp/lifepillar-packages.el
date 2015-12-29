43cf2ec74311d8d18f6cdc5ce15c2870aceb9845
Lifepillar <github@lifepillar.com>
2015-12-29 22:57:23 +0200

    Add Helm package, with preliminary configuration.

--- lisp/lifepillar-packages.el
+++ lisp/lifepillar-packages.el
@@ -14,6 +14,7 @@
     atom-dark-theme
     atom-one-dark-theme
     avy
+    helm
     solarized-theme
     which-key
     zenburn-theme)
@@ -62,6 +63,38 @@ Missing packages are installed automatically."
 (global-set-key (kbd "M-g g") 'avy-goto-line)
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
 (global-set-key (kbd "M-g e") 'avy-goto-word-0)
+
+;; Helm
+(require 'helm-config)
+(helm-mode 1)
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+(define-key global-map [remap find-file] 'helm-find-files)
+(define-key global-map [remap occur] 'helm-occur)
+(define-key global-map [remap list-buffers] 'helm-buffers-list)
+(define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+; (unless (boundp 'completion-in-region-function)
+;   (define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
+;   (define-key emacs-lisp-mode-map       [remap completion-at-point] 'helm-lisp-completion-at-point))
+
+; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+; (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+; (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+; (when (executable-find "curl")
+;   (setq helm-google-suggest-use-curl-p t))
+
+; (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+;       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+;       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+;       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+;       helm-ff-file-name-history-use-recentf t)
+
 
 (require 'which-key)
 (which-key-mode)
